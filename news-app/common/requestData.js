2d18e76b35e1424b1b8af98aa866056688b10ea5
QianGuoqing <qian_guoqing@yeah.net>
2018-04-14 13:52:53 +0800

    添加tab栏，以promise方式封装请求接口

---
+++ news-app/common/requestData.js
@@ -0,0 +1,57 @@
+/**
+ * 由于多个地方需要获取API数据，所以在一个公共模块中（此处的common文件夹）封装好获取API的操作
+ * 基于Promise
+ */
+
+function requestNewsList(type) {
+  return new Promise((resolve, reject) => {
+    wx.request({
+      url: 'https://test-miniprogram.com/api/news/list',
+      data: {
+        type: type
+      },
+      method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+      // header: {}, // 设置请求的 header
+      success: function(res){
+        // success
+        resolve(res)
+      },
+      fail: function(err) {
+        // fail
+        reject(err)
+      },
+      complete: function() {
+        // complete
+      }
+    })
+  })
+}
+
+function requestNewsDetail(news_id) {
+  return new Promise((resolve, reject) => {
+    wx.request({
+      url: 'https://test-miniprogram.com/api/news/detail',
+      data: {
+        id: news_id
+      },
+      method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+      // header: {}, // 设置请求的 header
+      success: function(res){
+        // success
+        resolve(res)
+      },
+      fail: function(err) {
+        // fail
+        reject(err)
+      },
+      complete: function() {
+        // complete
+      }
+    })
+  })
+}
+
+module.exports = {
+  requestNewsList: requestNewsList,
+  requestNewsDetail: requestNewsDetail
+}
---
+++ news-app/pages/index/index.js
@@ -0,0 +1,52 @@
+const requestData = require('../../common/requestData')
+const newsMap = {
+  '国内': 'gn',
+  '国际': 'gj',
+  '财经': 'cj',
+  '娱乐': 'yl',
+  '军事': 'js',
+  '体育': 'ty',
+  '其他': 'other'
+}
+
+Page({
+  data: {
+    tabList: ['国内', '国际', '财经', '娱乐', '军事', '体育', '其他'],
+    type: 'gn',
+    currentTab: 0,
+    topNews: '',
+    newsList: []
+  },
+  onLoad() {
+    this._getNewsData()
+  },
+  /**
+   * Tab栏切换，获取view上的text和index
+   * text用于获取tab上的值，和newsMap做映射
+   * index用于tab切换时给swiper组件的current属性赋值，以切换到不同的swiper-item
+   */
+  onSwitchTap(event) {
+    let tabItemText = event.currentTarget.dataset.tabItemText
+    let tabIndex = event.currentTarget.dataset.currentIndex
+    let type = newsMap[tabItemText]
+    this.setData({ 
+      type: type,
+      currentTab: tabIndex
+    })
+    this._getNewsData()
+  },
+  _getNewsData() {
+    requestData.requestNewsList(this.data.type).then(res => {
+      res = res.data
+      console.log(res.result);
+      this.setData({
+        newsList: res.result
+      })
+      this.setData({
+        topNews: this.data.newsList[0]
+      })
+    }).catch(err => {
+      console.log(err);
+    })
+  }
+})
